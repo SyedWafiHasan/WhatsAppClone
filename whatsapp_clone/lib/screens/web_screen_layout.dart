f8e118dcb524e62ff24789964a4ebbfe8025d2dd
Syed Wafi Hasan <syedwafihasan@gmail.com>
2022-11-14 19:27:54 +0530

    sab changa

--- whatsapp_clone/lib/screens/web_screen_layout.dart
+++ whatsapp_clone/lib/screens/web_screen_layout.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:whatsapp_clone/widgets/chat_list.dart';
 import 'package:whatsapp_clone/widgets/contacts_list.dart';
+import 'package:whatsapp_clone/widgets/web_chat_appbar.dart';
 import 'package:whatsapp_clone/widgets/web_profile_bar.dart';
 import 'package:whatsapp_clone/widgets/web_search_bar.dart';
 
@@ -31,6 +33,13 @@ class WebScreenLayout extends StatelessWidget {
                 image: AssetImage('assets/backgroundImage.png'),
                 fit: BoxFit.cover
               )
+            ),
+            child: Column(
+              children: const [
+                WebChatAppBar(),
+                ChatList(),
+                //Message Input Box
+              ],
             ),
           )
         ],
