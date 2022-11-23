046ac577cb9b84ffdbab868b4e1e9547f9aa6c73
Syed Wafi Hasan <syedwafihasan@gmail.com>
2022-11-23 22:22:52 +0530

    installed riverpod

---
+++ whatsapp_clone/lib/features/auth/controller/auth_controller.dart
@@ -0,0 +1,8 @@
+import 'package:whatsapp_clone/features/auth/repository/auth_repository.dart';
+
+class AuthController {
+  final AuthRepository authRepository;
+  AuthController({
+    required this.authRepository,
+  });
+}
--- whatsapp_clone/lib/features/auth/repository/auth_repository.dart
+++ whatsapp_clone/lib/features/auth/repository/auth_repository.dart
@@ -1,8 +1,16 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:whatsapp_clone/common/utils/utils.dart';
 import 'package:whatsapp_clone/features/auth/screens/otp_screen.dart';
+
+final authRepositoryProvider = Provider(
+  (ref) => AuthRepository(
+    firebaseAuth: FirebaseAuth.instance,
+    firebaseFirestore: FirebaseFirestore.instance,
+  ),
+);
 
 class AuthRepository {
   final FirebaseAuth firebaseAuth;
