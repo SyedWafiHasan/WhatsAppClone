e81752d8f777a6f6ddc97fc024654b7efe1b4ee3
Syed Wafi Hasan <syedwafihasan@gmail.com>
2022-11-27 16:11:53 +0530

    More changes to user info screen

--- whatsapp_clone/lib/features/auth/screens/user_information_screen.dart
+++ whatsapp_clone/lib/features/auth/screens/user_information_screen.dart
@@ -1,28 +1,62 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:whatsapp_clone/colors.dart';
 
-class UserInformationScreen extends StatelessWidget {
+class UserInformationScreen extends StatefulWidget {
   static const String routeName = '/user-information';
   const UserInformationScreen({super.key});
 
   @override
+  State<UserInformationScreen> createState() => _UserInformationScreenState();
+}
+
+class _UserInformationScreenState extends State<UserInformationScreen> {
+  final TextEditingController nameController = TextEditingController();
+
+  @override
+  void dispose() {
+    super.dispose();
+    nameController.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
     return Scaffold(
       body: SafeArea(
         child: Center(
           child: Column(
             children: [
               Stack(
-                children: const [
-                  CircleAvatar(
+                children: [
+                  const CircleAvatar(
                     backgroundColor: Colors.transparent,
-                    child: Icon(Icons.add_a_photo),
+                    backgroundImage: NetworkImage('https://picsum.photos/id/9/1920/1080'),
+                    radius: 64,
+                    //child: Icon(Icons.add_a_photo),
+                  ),
+                  Positioned(
+                    bottom: -10,
+                    left: 80,
+                    child: IconButton(
+                      onPressed: () {},
+                      icon: const Icon(Icons.add_a_photo),
+                    ),
                   ),
                 ],
               ),
-              IconButton(onPressed: () {}, icon: Icon(Icons.add_a_photo)),
+              Row(
+                children: [
+                  Container(
+                    width: size.width * 0.85,
+                    padding: const EdgeInsets.all(20),
+                    child: TextField(
+                      controller: nameController,
+                      decoration: InputDecoration(
+                        hintText: 'Enter your name',
+                      ),
+                    ),
+                  )
+                ],
+              )
             ],
           ),
         ),
